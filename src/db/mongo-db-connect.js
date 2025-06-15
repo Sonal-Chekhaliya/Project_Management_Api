0ea540eb2bd8bdba59b4976259a9461eb58b5eba
Sonal Chekhaliya <sonalchekhaliya2004@gmail.com>
2025-06-15 15:08:36 +0530

    sunday commit

--- src/db/mongo-db-connect.js
+++ src/db/mongo-db-connect.js
@@ -4,22 +4,23 @@ import { createAdmin } from "../seedData/createAdmin.js";
 
 dotenv.config();
 
-const username = process.env.MONGO_USERNAME;
+const username = process.env.MONGO_USER;
 const password = process.env.MONGO_PASSWORD;
 const clusterName = process.env.MONGO_CLUSTER_NAME;
-const dbName = process.env.MONGO_DB_NAME;
 
-const connection_string = `mongodb+srv://${username}:${encodeURIComponent(password)}@${clusterName}.fwvxail.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=ProjectManagement`;
-
+const connection_string = `mongodb+srv://${username}:${password}@${clusterName}.qmaep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//const connection_string = mongodb+srv://${username}:${password}@${clusterName}.llyjn6g.mongodb.net/learning?retryWrites=true&w=majority&appName=Cluster0;
 export const connectDB = async () => {
   try {
+  
     await mongoose.connect(connection_string, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       family: 4,
-    });
-    await createAdmin();
-    console.log('Connection successful');
+    }).then(async ()=>{  
+      await createAdmin();
+      console.log('Connection successfull')}
+    );
   } catch (error) {
     console.error("Error connecting to MongoDB Atlas:", error.message);
     throw error;
